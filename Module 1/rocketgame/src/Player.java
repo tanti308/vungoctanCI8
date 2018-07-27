f9c1c30b3b43fceb7673123f35d084b04e1fa032
tanti308 <tanvn.nevents@gmail.com>
2018-07-27 22:56:15 +0700

    BVN buoi 4 - Bai 1+2 B1: sau 1 khoang tgian tao 1 enemy B2: tao class player hinh tam giac (ve = drawPolygon)

---
+++ Module 1/rocketgame/src/Player.java
@@ -0,0 +1,13 @@
+import java.awt.image.BufferedImage;
+
+public class Player {
+
+    public BufferedImage image;
+
+    public int x;
+    public int y;
+
+    public int width;
+    public int height;
+
+}
